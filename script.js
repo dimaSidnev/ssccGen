5fe885347e6683737d9d0d3448807650f3c1f3e7
Dima Sidnev <sidnev.dimon@gmail.com>
2024-01-07 23:31:42 +0300

    релиз

--- script.js
+++ script.js
@@ -4,30 +4,68 @@ barcodeOptions = {
     height: 150,
 }
 
-//допиши логику для скачивания canvas и займись рефакторингом
-
 const input = document.querySelector("#input");
-const btn = document.querySelector("#btn");
+const barcode = document.querySelector("#barcode");
 const sscc = document.querySelector(".sscc");
-const organica = document.querySelector(".sscc__organica");
-const download = document.querySelector("#download");
+const logo = document.querySelector("#logo");
+const downloadBtn = document.querySelector("#download");
+const generateBtn = document.querySelector("#generate");
+const resetBtn = document.querySelector("#reset");
 
-function renderBarcode(){
-    download.classList.add("guide__download_red");
-    download.href = document.getElementsByTagName("canvas");
-    return JsBarcode("#barcode", input.value, barcodeOptions);
-
+function dataToURL(){
+    let canvas = document.getElementsByTagName("canvas");
+    return canvas[0].toDataURL("image/png");
 }
 
-function toImage(){
-    html2canvas(sscc).then(function(canvas) {
-        sscc.appendChild(canvas);
-    });
-    sscc.classList.add("sscc_hidden");
-}
 
-JsBarcode("#barcode", "(00)00000000000000000", barcodeOptions)
-
+generateBtn.addEventListener("click", toCanvas);
+downloadBtn.addEventListener("click", download);
+resetBtn.addEventListener("click", reset);
 sscc.addEventListener("click", () => { input.focus() });
-input.addEventListener("input", renderBarcode);
-download.addEventListener("click", toImage);
+input.addEventListener("input", generateBarcode);
+
+
+function hideSSCC(){
+    logo.classList.add("hidden");
+    barcode.classList.add("hidden");
+    input.classList.add("hidden");
+    generateBtn.classList.add("hidden");
+    downloadBtn.classList.remove("hidden");
+    resetBtn.classList.remove("hidden");
+}
+
+function unHideSSCC(){
+    logo.classList.remove("hidden");
+    barcode.classList.remove("hidden");
+    input.classList.remove("hidden");
+    generateBtn.classList.remove("hidden");
+    downloadBtn.classList.add("hidden");
+    resetBtn.classList.add("hidden");
+}
+
+function generateBarcode(){
+    return JsBarcode("#barcode", input.value, barcodeOptions);
+}
+
+function toCanvas(){
+    html2canvas(document.getElementById("capture")).then(function(childCanvas) {
+        sscc.appendChild(childCanvas);
+    })
+    hideSSCC();
+
+}
+
+function download(){
+    downloadBtn.href = dataToURL();
+}
+
+function reset(){
+    let canvas = document.getElementsByTagName("canvas");
+    canvas[0].remove();
+    input.value = "";
+    unHideSSCC();
+    JsBarcode("#barcode", "(00)00000000000000000", barcodeOptions);
+}
+
+
+JsBarcode("#barcode", "(00)00000000000000000", barcodeOptions);
